c08e5e677bee50c72e8d5e0e4bf4999bc512e3c8
Zach Green <zachjamesgreen@gmail.com>
2021-06-20 20:21:38 -0600

    remove line

--- database/database.go
+++ database/database.go
@@ -12,7 +12,6 @@ var DB *sql.DB
 
 func Start() {
 	connStr := fmt.Sprintf("host=%s user=%s dbname=musicplayer", os.Getenv("DB_HOST"), os.Getenv("DB_USERNAME"))
-	fmt.Println(connStr)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
