6974c7e11a7c6ec951a39f23dfffb3a6b979be76
Zach Green <zachjamesgreen@gmail.com>
2021-06-24 22:02:35 -0600

    change song struct to add artist and album. Changeone type

--- models/song.go
+++ models/song.go
@@ -5,20 +5,21 @@ import (
 	"fmt"
 	"log"
 	db "music/database"
-	"time"
 )
 
 type Song struct {
-	Id         int       `json:"id"`
-	Title      string    `json:"title"`
-	Track      int       `json:"track"`
-	Comment    string    `json:"comment"`
-	Year       int    `json:"year"`
-	LastPlayed time.Time `json:"last_played"`
-	ArtistId   int       `json:"artist_id"`
-	AlbumId    int       `json:"album_id"`
-	Path       string    `json:"path"`
-	Genre      Genre     `json:"genre"`
+	Id         int          `json:"id"`
+	Title      string       `json:"title"`
+	Track      int          `json:"track"`
+	Comment    string       `json:"comment"`
+	Year       int          `json:"year"`
+	LastPlayed sql.NullTime `json:"last_played"`
+	ArtistId   int          `json:"artist_id"`
+	AlbumId    int          `json:"album_id"`
+	Artist     string       `json:"artist"`
+	Album      string       `json:"album"`
+	Path       string       `json:"path"`
+	Genre      Genre        `json:"genre"`
 }
 
 type Genre struct {
@@ -36,7 +37,11 @@ func (g Genre) String() string {
 func GetSongs() []Song {
 	var song Song
 	var songs []Song
-	sqlStatment := `SELECT * FROM songs ORDER BY id`
+	sqlStatment := `
+	SELECT s.id, s.title, s.track, s.comment, s.year, s.last_played, s.path, s.genre, s.album_id, s.artist_id, al.title as album_title, ar.name
+	FROM songs AS s 
+	JOIN albums AS al ON s.album_id = al.id 
+	JOIN artists AS ar ON s.artist_id = ar.id ORDER BY s.id`
 	rows, err := db.DB.Query(sqlStatment)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -47,7 +52,7 @@ func GetSongs() []Song {
 	}
 	defer rows.Close()
 	for rows.Next() {
-		err := rows.Scan(&song.Id, &song.Title, &song.Track, &song.Comment, &song.AlbumId, &song.ArtistId, &song.Genre.Name, &song.Path)
+		err := rows.Scan(&song.Id, &song.Title, &song.Track, &song.Comment, &song.Year, &song.LastPlayed, &song.Path, &song.Genre.Name, &song.AlbumId, &song.ArtistId, &song.Album, &song.Artist)
 		if err != nil {
 			log.Fatal(err)
 		}
