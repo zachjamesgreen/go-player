ae7d2cf1807fcbdecc07f7f1033e2dfa78680397
Zach Green <zachjamesgreen@gmail.com>
2021-11-07 22:18:40 -0700

    add scripts

---
+++ scripts/refresh_database.sh
@@ -0,0 +1,15 @@
+set -e
+
+psql -U $DB_USERNAME -v "ON_ERROR_STOP=1" << EOF
+DROP DATABASE IF EXISTS musicplayer;
+DROP DATABASE IF EXISTS musicplayer_test;
+CREATE DATABASE musicplayer;
+CREATE DATABASE musicplayer_test;
+EOF
+
+# Run migrations on dev database
+goose -dir ./db/migrations postgres "host=$DB_HOST user=$DB_USERNAME dbname=musicplayer" up
+# Run migrations on test database
+goose -dir ./db/migrations postgres "host=$DB_HOST user=$DB_USERNAME dbname=musicplayer_test" up
+
+# run script for fixtures when i add fixtures
